9b9009b11b8e891d5fb43dcc8158c7de040fd110
ayush2725 <67007088+ayush2725@users.noreply.github.com>
2022-07-02 00:29:36 -0700

    Update Remove_Nth_Node_From_Last_1.py

--- Remove_Nth_Node_From_Last_1.py
+++ Remove_Nth_Node_From_Last_1.py
@@ -1,1 +1,30 @@
-
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        
+        dummy_node = ListNode(next = head)
+        
+        len_of_list = 0
+        
+        curr_node = head
+        
+        while curr_node != None:
+            
+            len_of_list += 1
+            curr_node = curr_node.next
+        
+        prev_node = dummy_node
+        curr_node = head
+        
+        for i in range(len_of_list - n):
+            
+            prev_node = prev_node.next
+            curr_node = curr_node.next
+        
+        prev_node.next = curr_node.next
+        
+        return dummy_node.next
