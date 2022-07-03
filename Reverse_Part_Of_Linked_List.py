861802c171be4a66048907c35946ba57f595d2cd
ayush2725 <67007088+ayush2725@users.noreply.github.com>
2022-07-02 18:51:28 -0700

    Create Reverse_Part_Of_Linked_List.py

---
+++ Reverse_Part_Of_Linked_List.py
@@ -0,0 +1,52 @@
+'''
+AUthor: Ayush Singh
+
+Question:
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position 
+left to position right, and return the reversed list.
+
+Logic:
+We'll go to the node just before the node to start reversing from and we'll reverse it just like we revese a full linked list until we reach
+the node left + right - 1. Once that is done, the node from which reversing started will be pointing to None and the node until which reversing
+happened will be the current node. We'll just adjust their next pointers and we'll be done. We'll also be keeping a dummy node for the case
+in which we have to reverse the head as well
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+'''
+
+# 
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        
+        dummy_node = ListNode(next = head)
+        
+        left_node = dummy_node
+        
+        for i in range (left - 1):
+            
+            left_node = left_node.next
+        
+        prev_node = None
+        curr_node = left_node.next
+        
+        for i in range (right - left + 1):
+            
+            future_node = curr_node.next
+            
+            curr_node.next = prev_node
+            
+            prev_node = curr_node
+            
+            curr_node = future_node
+        
+        left_node.next.next = curr_node
+        
+        left_node.next = prev_node
+        
+        return dummy_node.next
