543856c5f766b28ca0bf2df29b14937a95e4ae85
ayush2725 <67007088+ayush2725@users.noreply.github.com>
2022-07-06 04:30:19 -0700

    Update Postorder_Transversal.py

--- Postorder_Transversal.py
+++ Postorder_Transversal.py
@@ -1,9 +1,24 @@
+'''
+Author: Ayush Singh
+
+Question:
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Logic: 
+Postorder transversal is Node.left, Node.right, Node. We'll recursively call the function on Node.left first, then on Node.right, and we'll 
+finally add that value to the array
+
+Time Complexity: 
+Space Complexity:
+'''
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
 class Solution(object):
     def postorderTraversal(self, root):
         
