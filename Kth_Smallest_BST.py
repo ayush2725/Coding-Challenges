de0f45c1c5b10c7e1c23eb69bda5634341f2530f
ayush2725 <67007088+ayush2725@users.noreply.github.com>
2022-07-07 03:27:56 -0700

    Update Kth_Smallest_BST.py

--- Kth_Smallest_BST.py
+++ Kth_Smallest_BST.py
@@ -1,9 +1,27 @@
+'''
+Author: Ayush Singh
+
+Question:
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+Logic:
+We'll perform an in-order transversal of the tree. This'll return us an array in the increasing order. Starting from 0th index, we'll go to 
+(k-1)th index, that will be the kth smallest element.
+
+Time Complexity: O(n)
+Space Complexity: O(d)    d is the depth of the tree
+'''
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    
+    def __init__(self, val=0, left=None, right=None):
+        
+        self.val = val
+        self.left = left
+        self.right = right
+        
+# Definition of the Solution class
 class Solution(object):
     def kthSmallest(self, root, k):
         
