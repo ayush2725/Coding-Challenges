9125c8fb455df0a2030a380a31629187e877133c
ayush2725 <67007088+ayush2725@users.noreply.github.com>
2022-07-07 03:20:50 -0700

    Update Level_Order_Transversal.py

--- Level_Order_Transversal.py
+++ Level_Order_Transversal.py
@@ -1,9 +1,27 @@
+'''
+Author: Ayush Singh
+
+Question:
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Logic:
+We'll be maintaining a queue. Starting from appending the root, we'll keep on appending the peek() element's left child, then right child,
+this'll go left to right for each level of the binary tree
+
+Time complexity: O(n)
+Space complexity: O(n)
+'''
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    
+    def __init__(self, val=0, left=None, right=None):
+        
+        self.val = val
+        self.left = left
+        self.right = right
+        
+# Definition of the Solution class
 class Solution(object):
     
     def levelOrder(self, root):
@@ -26,3 +44,5 @@ class Solution(object):
                 lst.append(lst[0].right)
             
             del lst[0]
+        
+        return root
